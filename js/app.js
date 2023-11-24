10ba28f8c54b25ba5fc40cd226ef94a6bfdd3241
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-24 18:53:14 -0300

    Lógica do sorteio concluída.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,26 @@ function adicionar() {
     listaDeAmigos.push(amigo);
 
     let lista = document.getElementById("lista-amigos");
-    lista.innerHTML = "";
-    lista.innerHTML += listaDeAmigos;
+    lista.innerHTML = listaDeAmigos;
+}
+
+function sortear() {
+    listaDeAmigos.sort();
+    let amigosEmbaralhados = document.getElementById("lista-sorteio");
+    let i = 0;
+    while (i < listaDeAmigos.length) {
+        for (let j = 0; j < listaDeAmigos.length; j++) {
+            if (listaDeAmigos[i] != listaDeAmigos[j] && i < listaDeAmigos.length) {
+                console.log(i + " " + listaDeAmigos[i] + " != " + listaDeAmigos[j] + " " + j);
+                amigosEmbaralhados.innerHTML += listaDeAmigos[i] + " -> " + listaDeAmigos[j] + "<br>";
+                i++;
+            }
+        }
+    }
+}
+
+function reiniciar() {
+    document.getElementById("lista-amigos").innerHTML = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
+    document.getElementById("nome-amigo").value = "";
 }
