03b667752eed9729f72dd946de61f450ede8e20d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-25 08:19:27 -0300

    Lógica para sortear os mesmos amigos apenas uma vez

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var listaDeAmigos = [];
+var amigosForamSorteados = false;
 
 function adicionar() {
     let amigo = document.getElementById("nome-amigo").value;
@@ -16,17 +17,22 @@ function adicionar() {
 
 function sortear() {
     if (listaDeAmigos.length != 0) {
-        listaDeAmigos.sort();
-        let amigosEmbaralhados = document.getElementById("lista-sorteio");
-        let i = 0;
-        while (i < listaDeAmigos.length) {
-            for (let j = 0; j < listaDeAmigos.length; j++) {
-                if (listaDeAmigos[i] != listaDeAmigos[j] && i < listaDeAmigos.length) {
-                    console.log(i + " " + listaDeAmigos[i] + " != " + listaDeAmigos[j] + " " + j);
-                    amigosEmbaralhados.innerHTML += listaDeAmigos[i] + " -> " + listaDeAmigos[j] + "<br>";
-                    i++;
+        if (amigosForamSorteados == false) {
+            listaDeAmigos.sort();
+            let amigosEmbaralhados = document.getElementById("lista-sorteio");
+            let i = 0;
+            while (i < listaDeAmigos.length) {
+                for (let j = 0; j < listaDeAmigos.length; j++) {
+                    if (listaDeAmigos[i] != listaDeAmigos[j] && i < listaDeAmigos.length) {
+                        console.log(i + " " + listaDeAmigos[i] + " != " + listaDeAmigos[j] + " " + j);
+                        amigosEmbaralhados.innerHTML += listaDeAmigos[i] + " -> " + listaDeAmigos[j] + "<br>";
+                        i++;
+                    }
                 }
             }
+            amigosForamSorteados = true;
+        } else {
+            alert("Amigos já foram sorteados, reinicie o jogo para sortear novamente!!");
         }
     } else {
         alert("Adicione amigos para poder sortear!!");
@@ -38,4 +44,5 @@ function reiniciar() {
     document.getElementById("lista-amigos").innerHTML = "";
     document.getElementById("lista-sorteio").innerHTML = "";
     document.getElementById("nome-amigo").value = "";
+    amigosForamSorteados = false;
 }
